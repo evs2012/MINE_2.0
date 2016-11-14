10078ce48da7459f6665789e64ad3d180321c030
ntianasakioti <nsaki001@odu.edu>
2016-11-14 12:17:39 -0500

    final QT code

--- Milestone5/Milestone_5/mainwindow.cpp
+++ Milestone5/Milestone_5/mainwindow.cpp
@@ -18,7 +18,7 @@ MainWindow::MainWindow(QWidget *parent) :
     if (i ==1)
     {
         QMessageBox failed_open;
-        failed_open.setText("Error: Opening Failed.");
+        failed_open.setText("Error: Opening Comport Failed.");
         failed_open.exec();
     }
 
@@ -43,22 +43,71 @@ void MainWindow::on_TransferTest_clicked()
             QByteArray temp = fileName.toLatin1();
             bmp_file test_image;
             test_image = bmp_file(temp.data());
+            unsigned char *data = test_image.getFileData();
+
+
+            int result = RS232_SendBuf(comport_number, data,76801);
+
+            if( result == -1)
+            {
+                QMessageBox failed_send;
+                failed_send.setText("Error: Sending Failed.");
+                failed_send.exec();
+
+            }
         }
-
-
 }
 
 void MainWindow::on_TransferOverlay_clicked()
 {
+    QString fileName = QFileDialog::getOpenFileName(
+                    this,
+                    tr("Open Bitmap"),
+                    "C://", /// I would prefer this to be %HOMEDRIVE% & %HOMEPATH%
+                    "Bitmap Images (*.bmp);;All Files (*.*)"
+                    );
+        /// QMessageBox::information(this,tr("File Name"),fileName); /// Just to prove it is working
+        if(fileName != "")
+        {
+            QByteArray temp = fileName.toLatin1();
+            bmp_file overlay_image;
+            overlay_image = bmp_file(temp.data());
+            unsigned char *data = overlay_image.getFileData();
 
+            int result = RS232_SendBuf(comport_number, data,76801);
+
+            if( result == -1)
+            {
+                QMessageBox failed_send;
+                failed_send.setText("Error: Sending Failed.");
+                failed_send.exec();
+            }
+
+        }
 }
 
 void MainWindow::on_BrightnessSlider_valueChanged(int value)
 {
+    int opResult = RS232_SendByte(comport_number,(uint8_t)value);
+
+    if(opResult == 1)
+    {
+        QMessageBox failed_send;
+        failed_send.setText("Error: Sending Failed.");
+        failed_send.exec();
+    }
 
 }
 
 void MainWindow::on_ContrastSlider_valueChanged(int value)
 {
+    int opResult = RS232_SendByte(comport_number,(uint8_t)value);
+
+    if(opResult == 1)
+    {
+        QMessageBox failed_send;
+        failed_send.setText("Error: Sending Failed.");
+        failed_send.exec();
+    }
 
 }
